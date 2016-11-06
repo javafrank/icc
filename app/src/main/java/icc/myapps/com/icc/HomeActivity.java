234e19c484b52c35f423f864d45b1c0c623ae6be
javafrank <contrerasfrank@gmail.com>
2016-11-06 18:29:59 -0300

    - Hacer que funcione la app nuevamente - Crear iconos de la app - Hacer que el icono de notificacion aparezca permanentemente en la app y pueda cambiar

--- app/src/main/java/icc/myapps/com/icc/HomeActivity.java
+++ app/src/main/java/icc/myapps/com/icc/HomeActivity.java
@@ -1,7 +1,13 @@
 package icc.myapps.com.icc;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +28,8 @@ public class HomeActivity extends AppCompatActivity {
     private TextView connectedText;
     private Button verifyButton;
 
+    NotificationCompat.Builder mBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,12 +45,58 @@ public class HomeActivity extends AppCompatActivity {
                 verifyInternetConnection();
             }
         });
+
+        createAndDisplayNotification();
     }
 
     private void verifyInternetConnection() {
         connectedText.setText(getResources().getString(R.string.verifying));
         connectedText.setTextColor(getResources().getColor(R.color.verifying));
         new MyAsyncTask().execute();
+    }
+
+    private void createAndDisplayNotification() {
+        mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_on)
+                        .setContentTitle(getResources().getString(R.string.icc_status))
+                        .setContentText(getResources().getString(R.string.connected))
+                        .setOngoing(true);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, HomeActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(HomeActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
+    }
+
+    public void updateNotification(boolean connected) {
+        int textId = (connected) ? R.string.connected : R.string.disconnected;
+        int iconId = (connected) ? R.drawable.ic_on : R.drawable.ic_off;
+
+        mBuilder.setContentText(getString(textId));
+        mBuilder.setSmallIcon(iconId);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(
+                0,
+                mBuilder.build());
     }
 
     class MyAsyncTask extends AsyncTask<Void, Void, Boolean> {
@@ -60,29 +114,10 @@ public class HomeActivity extends AppCompatActivity {
                 connectedText.setText(getResources().getString(R.string.disconnected));
                 connectedText.setTextColor(getResources().getColor(R.color.disconnected));
             }
+            updateNotification(aBoolean);
         }
 
         public boolean isOnline() {
-            /*Runtime runtime = Runtime.getRuntime();
-            try {
-
-                Process ipProcess = runtime.exec("ping -c 1 www.google.com");
-                int     exitValue = ipProcess.waitFor();
-                return (exitValue == 0);
-
-            } catch (IOException e)          { e.printStackTrace(); }
-            catch (InterruptedException e) { e.printStackTrace(); }*/
-
-            /*try {
-                if (InetAddress.getByName("www.google.com").isReachable(6000)) {
-                    return true;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return false;*/
-
             try {
                 final URL url = new URL("http://www.google.com");
                 final URLConnection conn = url.openConnection();
@@ -92,25 +127,6 @@ public class HomeActivity extends AppCompatActivity {
                 throw new RuntimeException(e);
             } catch (IOException e) {
                 return false;
-            }
-        }
-
-        public class NetTask extends AsyncTask<String, Integer, String>
-        {
-            @Override
-            protected String doInBackground(String... params)
-            {
-                InetAddress addr = null;
-                try
-                {
-                    addr = InetAddress.getByName(params[0]);
-                }
-
-                catch (UnknownHostException e)
-                {
-                    e.printStackTrace();
-                }
-                return addr.getHostAddress();
             }
         }
     }
